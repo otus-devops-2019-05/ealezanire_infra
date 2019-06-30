fd868235b8ad9daf94e6b62ab021c8703c4042df
ealezanire <ealezanire@gmail.com>
2019-06-30 18:46:56 +0300

    Промежуточный коммит, были сделаны все подготовительные задачи перед самостоятельной работой.

---
+++ packer/files/install_puma.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+DIRECTORY='/home/appuser'
+
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+echo $DIRECTORY
+if [ -d "$DIRECTORY" ]; then
+        cd $DIRECTORY
+else
+        mkdir "$DIRECTORY"
+        cd $DIRECTORY
+fi
+
+set -e
+
+APP_DIR=${1:-$HOME}
+
+git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
+cd $APP_DIR/reddit
+bundle install
+
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+sudo systemctl start puma
+sudo systemctl enable puma
+
+
+
+
